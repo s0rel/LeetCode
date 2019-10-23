02465f50ba7a6f66e74ab02b7555457ddddcc060
s0rel <sorelllllllll@gmail.com>
2019-10-23 21:22:51 +0800

    Add 6~9

---
+++ src/org/sorel/leetcode/p0006/ZigZagConversion.java
@@ -0,0 +1,32 @@
+package org.sorel.leetcode.p0006;
+
+public class ZigZagConversion {
+    public String convert(String s, int numRows) {
+        StringBuilder[] arr = new StringBuilder[numRows];
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < numRows; i++) {
+            arr[i] = new StringBuilder();
+        }
+
+        int len = s.length();
+        for (int i = 0; i < len; ) {
+            // 竖直向下操作
+            for (int j = 0; j < numRows && i < len; j++) {
+                arr[j].append(charArray[i]);
+                i++;
+            }
+
+            // 沿对角线向上操作
+            for (int j = numRows - 2; j > 0 && i < len; j--) {
+                arr[j].append(charArray[i]);
+                i++;
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (StringBuilder temp : arr) {
+            res.append(temp);
+        }
+        return res.toString();
+    }
+}
