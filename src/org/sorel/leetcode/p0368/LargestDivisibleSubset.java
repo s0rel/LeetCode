c41f690fd1693576d4d3d0da045777ef4fa4497e
s0rel <sorelllllllll@gmail.com>
2019-11-02 08:36:31 +0800

    Refactor

--- src/org/sorel/leetcode/p0368/LargestDivisibleSubset.java
+++ src/org/sorel/leetcode/p0368/LargestDivisibleSubset.java
@@ -2,37 +2,44 @@ package org.sorel.leetcode.p0368;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class LargestDivisibleSubset {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int len = nums.length;
-        int[] cnt = new int[len];
-        int[] pre = new int[len];
+        List<Integer> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+
         Arrays.sort(nums);
-        int max = 0, index = -1;
+        int len = nums.length;
+        int[] dp = new int[len];
         for (int i = 0; i < len; i++) {
-            cnt[i] = 1;
-            pre[i] = -1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (nums[i] % nums[j] == 0) {
-                    if (1 + cnt[j] > cnt[i]) {
-                        cnt[i] = cnt[j] + 1;
-                        pre[i] = j;
-                    }
-                }
-            }
-            if (cnt[i] > max) {
-                max = cnt[i];
-                index = i;
+            dp[i] += 1;
+        }
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0)
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
             }
         }
 
-        List<Integer> res = new ArrayList<>();
-        while (index != -1) {
-            res.add(nums[index]);
-            index = pre[index];
+        int maxIndex = 0;
+        for (int i = 1; i < len; i++) {
+            maxIndex = dp[i] > dp[maxIndex] ? i : maxIndex;
         }
+
+        int max = nums[maxIndex];
+        for (int i = maxIndex; i >= 0; i--) {
+            if (max % nums[i] == 0 && dp[maxIndex] - dp[i] <= 1) {
+                res.add(nums[i]);
+                max = nums[i];
+                maxIndex = i;
+            }
+        }
+        Collections.sort(res);
         return res;
     }
 }
