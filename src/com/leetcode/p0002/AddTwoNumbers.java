ce60b4da68ae6006ff85a957d0fa6f4c8e33957c
s0rel <sorelllllllll@gmail.com>
2022-04-21 09:31:25 +0800

    Update solution to problem 2

--- src/com/leetcode/p0002/AddTwoNumbers.java
+++ src/com/leetcode/p0002/AddTwoNumbers.java
@@ -15,8 +15,8 @@ public class AddTwoNumbers {
             flag = sum / 10;
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            l1 = (l1 == null) ? l1 : l1.next;
-            l2 = (l2 == null) ? l2 : l2.next;
+            l1 = (l1 != null) ? l1.next : l1;
+            l2 = (l2 != null) ? l2.next : l2;
         }
 
         // 处理最高位有进位的情况
