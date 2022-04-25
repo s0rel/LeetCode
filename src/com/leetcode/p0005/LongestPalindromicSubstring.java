7944816b346f20f0296c948563fa968e6f9f31b6
s0rel <sorelllllllll@gmail.com>
2022-04-25 09:35:13 +0800

    Update solution to problem 5

--- src/com/leetcode/p0005/LongestPalindromicSubstring.java
+++ src/com/leetcode/p0005/LongestPalindromicSubstring.java
@@ -1,22 +1,16 @@
 package com.leetcode.p0005;
 
-/**
- * 动态规划
- */
 public class LongestPalindromicSubstring {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
-        }
-
         int len = s.length();
-        String res = "";
         boolean[][] dp = new boolean[len][len];
+        String res = "";
         int max = 0;
         for (int j = 0; j < len; j++) {
             for (int i = 0; i <= j; i++) {
                 boolean flag = s.charAt(i) == s.charAt(j);
-                dp[i][j] = j - i > 2 ? dp[i + 1][j - 1] && flag : flag;
+                // dp[i][j] 表示 s.substring(i, j + 1) 是不是回文
+                dp[i][j] = j - i > 2 ? flag && dp[i + 1][j - 1] : flag;
 
                 if (dp[i][j] && j - i + 1 > max) {
                     max = j - i + 1;
