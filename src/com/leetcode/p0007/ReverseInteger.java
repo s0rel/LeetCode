13542c714c7df3f35c151ec9fdbb340f193f31c9
s0rel <sorelllllllll@gmail.com>
2022-04-26 01:11:35 +0800

    Update solution to problem 7

--- src/com/leetcode/p0007/ReverseInteger.java
+++ src/com/leetcode/p0007/ReverseInteger.java
@@ -4,6 +4,7 @@ public class ReverseInteger {
     public int reverse(int x) {
         long res = 0;
         while (x != 0) {
+            // 不需要区分 x 的正负
             res = res * 10 + x % 10;
             x /= 10;
         }
