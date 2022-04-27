88205c38d67e7d1deed1a7c1296816f50070347a
s0rel <sorelllllllll@gmail.com>
2022-04-27 09:22:55 +0800

    Update solution to problem 11

--- src/com/leetcode/p0011/ContainerWithMostWater.java
+++ src/com/leetcode/p0011/ContainerWithMostWater.java
@@ -2,7 +2,9 @@ package com.leetcode.p0011;
 
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
-        int res = 0, l = 0, r = height.length - 1;
+        int res = 0;
+        int l = 0;
+        int r = height.length - 1;
         while (l < r) {
             res = Math.max(Math.min(height[l], height[r]) * (r - l), res);
             if (height[l] <= height[r]) {
