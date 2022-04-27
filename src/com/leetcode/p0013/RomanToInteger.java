e029177ad085c07d61edb73d7f8e45b837b6de6f
s0rel <sorelllllllll@gmail.com>
2022-04-27 09:43:08 +0800

    Update solution to problem 13

--- src/com/leetcode/p0013/RomanToInteger.java
+++ src/com/leetcode/p0013/RomanToInteger.java
@@ -14,7 +14,8 @@ public class RomanToInteger {
         map.put('D', 500);
         map.put('M', 1000);
 
-        int res = 0, len = s.length();
+        int res = 0;
+        int len = s.length();
         for (int i = 0; i < len; i++) {
             if (i > 0 && map.get(s.charAt(i)) > map.get(s.charAt(i - 1))) {
                 res += map.get(s.charAt(i)) - 2 * map.get(s.charAt(i - 1));
