d8014cfe7c32c6704da09e295e9f60ce72de0598
s0rel <sorelllllllll@gmail.com>
2022-05-02 16:16:56 +0800

    Update solution to problem 21

--- src/com/leetcode/p0021/MergeTwoSortedLists.java
+++ src/com/leetcode/p0021/MergeTwoSortedLists.java
@@ -4,15 +4,8 @@ import com.leetcode.structures.ListNode;
 
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) {
-            return null;
-        } else if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
-        }
-
-        ListNode head = new ListNode(-1), curr = head;
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
         while (l1 != null && l2 != null) {
             if (l1.val <= l2.val) {
                 curr.next = l1;
