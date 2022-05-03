77a5d114de533bc48c4f0eca4c6ca9e9a06c8084
s0rel <sorelllllllll@gmail.com>
2022-05-03 11:08:27 +0800

    Update solution to problem 27

--- src/com/leetcode/p0027/RemoveElement.java
+++ src/com/leetcode/p0027/RemoveElement.java
@@ -2,15 +2,17 @@ package com.leetcode.p0027;
 
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        int idx = 0, len = nums.length;
-        while (idx < len) {
-            if (nums[idx] == val) {
-                nums[idx] = nums[len - 1];
-                len--;
+        int l = 0;
+        int r = 0;
+        int len = nums.length;
+        while (r < len) {
+            if (nums[r] == val) {
+                r++;
             } else {
-                idx++;
+                // 由于左边的指针每次赋值后会自增，所以最终的结果只需要返回左边的指针即可，不需要加 1
+                nums[l++] = nums[r++];
             }
         }
-        return len;
+        return l;
     }
 }
