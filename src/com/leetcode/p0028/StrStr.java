e34ac4072879596af9cbcd9219ce7d3aa9b7e266
s0rel <sorelllllllll@gmail.com>
2022-05-03 11:27:36 +0800

    Update solution to problem 28

---
+++ src/com/leetcode/p0028/StrStr.java
@@ -0,0 +1,28 @@
+package com.leetcode.p0028;
+
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        if (needle == null || needle.length() == 0) {
+            return 0;
+        }
+        if (haystack == null || haystack.length() == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < haystack.length(); i++) {
+            if (i + needle.length() > haystack.length()) {
+                break;
+            }
+
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    break;
+                }
+                if (j == needle.length() - 1) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/com/leetcode/p0028/StrStr.md
@@ -0,0 +1,3 @@
+### 分析
+
+遍历 haystack，以 haystack 当前遍历到的字符为起点，判断是否包含 needle 
