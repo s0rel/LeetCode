c2dc51240eafecf340f7d7dec24eff7cd801ade7
s0rel <sorelllllllll@gmail.com>
2022-05-04 11:34:37 +0800

    Update solution to problem 34

--- src/com/leetcode/p0034/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/com/leetcode/p0034/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -2,33 +2,52 @@ package com.leetcode.p0034;
 
 public class FindFirstAndLastPositionOfElementInSortedArray {
     public int[] searchRange(int[] nums, int target) {
+        int[] result = {-1, -1};
         if (nums == null || nums.length == 0) {
-            return new int[]{-1, -1};
+            return result;
         }
 
-        int l = 0, r = nums.length - 1;
-        int[] res = new int[2];
-        while (l < r) {
+        result[0] = findStartPosition(nums, target);
+        result[1] = findEndPosition(nums, target);
+        return result;
+    }
+
+    public int findStartPosition(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int start = -1;
+
+        while (l <= r) {
             int m = l + (r - l) / 2;
-            if (target > nums[m]) {
+            if (nums[m] == target) {
+                start = m;
+                r = m - 1;
+            } else if (target > nums[m]) {
                 l = m + 1;
             } else {
-                r = m;
-            }
-        }
-        res[0] = (target == nums[l]) ? l : -1;
-
-        r = nums.length - 1;
-        while (l < r) {
-            int m = l + (r - l) / 2 + 1;
-            if (target < nums[m]) {
                 r = m - 1;
-            } else {
-                l = m;
             }
         }
-        res[1] = (target == nums[r]) ? r : -1;
 
-        return res;
+        return start;
+    }
+
+    public int findEndPosition(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int end = -1;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                end = m;
+                l = m + 1;
+            } else if (target > nums[m])
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+
+        return end;
     }
 }
