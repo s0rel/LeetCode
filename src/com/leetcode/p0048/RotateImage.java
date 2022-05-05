b63098c8df836e4e76b0efd8caf5464577b98a07
s0rel <sorelllllllll@gmail.com>
2022-05-05 10:06:44 +0800

    Update solution to problem 48

--- src/com/leetcode/p0048/RotateImage.java
+++ src/com/leetcode/p0048/RotateImage.java
@@ -2,25 +2,33 @@ package com.leetcode.p0048;
 
 public class RotateImage {
     public void rotate(int[][] matrix) {
-        if (matrix == null || matrix[0] == null || matrix[0].length == 0) {
-            return;
-        }
+        int length = matrix.length;
+        int width = matrix[0].length;
+        int l = 0;
+        int r = length - 1;
 
-        int l = 0, r = matrix.length - 1;
         while (l < r) {
-            int[] temp = matrix[l];
-            matrix[l] = matrix[r];
-            matrix[r] = temp;
+            swapArray(matrix, l, r);
             l++;
             r--;
         }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i + 1; j < matrix[i].length; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < width; j++) {
+                swap(matrix, i, j);
             }
         }
+    }
+
+    private void swapArray(int[][] matrix, int i, int j) {
+        int[] temp = matrix[i];
+        matrix[i] = matrix[j];
+        matrix[j] = temp;
+    }
+
+    private void swap(int[][] matrix, int i, int j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
     }
 }
