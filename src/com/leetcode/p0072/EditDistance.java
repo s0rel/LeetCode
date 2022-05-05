3ba57f3104ef0f8ae1bd000ceed9dc211c8ba2c4
s0rel <sorelllllllll@gmail.com>
2022-05-05 16:33:32 +0800

    Update solution to problem 72

--- src/com/leetcode/p0072/EditDistance.java
+++ src/com/leetcode/p0072/EditDistance.java
@@ -2,32 +2,28 @@ package com.leetcode.p0072;
 
 public class EditDistance {
     public int minDistance(String word1, String word2) {
-        int m = word1.length(), n = word2.length();
-        if (m == 0) {
-            return n;
+        int len1 = word1.length();
+        int len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 0; i < len1 + 1; i++) {
+            dp[i][0] = i;
         }
-        if (n == 0) {
-            return m;
-        }
-
-        char[] str1 = word1.toCharArray(), str2 = word2.toCharArray();
-        int[][] table = new int[m + 1][n + 1];
-        for (int i = 0; i < m + 1; i++) {
-            table[i][0] = i;
-        }
-        for (int j = 0; j < n + 1; j++) {
-            table[0][j] = j;
+        for (int j = 0; j < len2 + 1; j++) {
+            dp[0][j] = j;
         }
 
-        for (int i = 1; i < m + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                int cost = 0;
-                if (str1[i - 1] != str2[j - 1]) {
-                    cost = 1;
+        for (int i = 1; i < len1 + 1; i++) {
+            for (int j = 1; j < len2 + 1; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], // replace
+                            Math.min(dp[i][j - 1], // delete
+                                    dp[i - 1][j]) // insert
+                    ) + 1;
                 }
-                table[i][j] = Math.min(Math.min(table[i - 1][j] + 1, table[i][j - 1] + 1), table[i - 1][j - 1] + cost);
             }
         }
-        return table[m][n];
+        return dp[len1][len2];
     }
 }
