d633c78a8ca8bcd83b2616397cc6c230fea381e6
s0rel <sorelllllllll@gmail.com>
2022-05-05 11:05:12 +0800

    Update solution to problem 53

--- src/com/leetcode/p0053/MaximumSubarray.java
+++ src/com/leetcode/p0053/MaximumSubarray.java
@@ -2,11 +2,8 @@ package com.leetcode.p0053;
 
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int sum = 0, max = Integer.MIN_VALUE;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
         for (int num : nums) {
             sum = Math.max(sum + num, num);
             max = Math.max(max, sum);
