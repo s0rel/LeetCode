38ac861222c0d33b7924396048e2a44171b10c88
s0rel <sorelllllllll@gmail.com>
2022-05-05 09:28:12 +0800

    Update solution to problem 46

--- src/com/leetcode/p0022/GenerateParentheses.java
+++ src/com/leetcode/p0022/GenerateParentheses.java
@@ -6,21 +6,21 @@ import java.util.List;
 public class GenerateParentheses {
     public List<String> generateParenthesis(int n) {
         List<String> res = new ArrayList<>();
-        backtrack(res, "", 0, 0, n);
+        backtracking(res, "", 0, 0, n);
         return res;
     }
 
-    private void backtrack(List<String> res, String s, int open, int close, int n) {
+    private void backtracking(List<String> res, String s, int open, int close, int n) {
         if (s.length() == 2 * n) {
             res.add(s);
             return;
         }
 
         if (open < n) {
-            backtrack(res, s + "(", open + 1, close, n);
+            backtracking(res, s + "(", open + 1, close, n);
         }
         if (close < open) {
-            backtrack(res, s + ")", open, close + 1, n);
+            backtracking(res, s + ")", open, close + 1, n);
         }
     }
 }
--- src/com/leetcode/p0046/Permutations.java
+++ src/com/leetcode/p0046/Permutations.java
@@ -6,24 +6,22 @@ import java.util.List;
 public class Permutations {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        res.add(new ArrayList<>());
-        return backtracking(res, nums, 0);
+        backtracking(res, new ArrayList<>(), nums);
+        return res;
     }
 
-    private List<List<Integer>> backtracking(List<List<Integer>> res, int[] nums, int pos) {
-        if (pos == nums.length) {
-            return res;
-        }
-
-        List<List<Integer>> newResult = new ArrayList<>();
-        for (List<Integer> eachList : res) {
-            for (int i = 0; i <= eachList.size(); i++) { // 这里要取 "="
-                List<Integer> newList = new ArrayList<>(eachList);
-                newList.add(i, nums[pos]);
-                newResult.add(newList);
+    private void backtracking(List<List<Integer>> res, List<Integer> curr, int[] nums) {
+        if (curr.size() == nums.length) {
+            res.add(new ArrayList<>(curr));
+        } else {
+            for (int num : nums) {
+                if (curr.contains(num)) {
+                    continue;
+                }
+                curr.add(num);
+                backtracking(res, curr, nums);
+                curr.remove(curr.size() - 1);
             }
         }
-        res = newResult;
-        return backtracking(res, nums, pos + 1);
     }
 }
