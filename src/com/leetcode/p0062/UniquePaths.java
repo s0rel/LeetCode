33deeb9f3ae0c0bb3d88db212df66f8f6a429990
s0rel <sorelllllllll@gmail.com>
2022-05-05 15:01:30 +0800

    Update solution to problem 62

--- src/com/leetcode/p0062/UniquePaths.java
+++ src/com/leetcode/p0062/UniquePaths.java
@@ -1,13 +1,15 @@
 package com.leetcode.p0062;
 
-public class UniquePaths_I {
+public class UniquePaths {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == 0 || j == 0) {
+                    // 基线条件
                     dp[i][j] = 1;
                 } else {
+                    // 递推式
                     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
                 }
             }
