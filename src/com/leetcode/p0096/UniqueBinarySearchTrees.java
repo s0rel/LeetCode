772414fa1d2a20b0937deec49fe1a45edd7eb3a7
s0rel <sorelllllllll@gmail.com>
2022-05-06 14:22:36 +0800

    Update solution to problem 96

--- src/com/leetcode/p0096/UniqueBinarySearchTrees.java
+++ src/com/leetcode/p0096/UniqueBinarySearchTrees.java
@@ -5,8 +5,8 @@ public class UniqueBinarySearchTrees {
         int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
 
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 1; j <= i; ++j) {
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
                 dp[i] += dp[j - 1] * dp[i - j];
             }
         }
